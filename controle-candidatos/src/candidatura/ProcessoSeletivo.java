78271bf34fcea2c4bb3477b4f9cf46be5a71ee7a
guigx <guigo211103@gmail.com>
2024-04-24 16:55:39 -0300

    Fazendo ligações para cada candidato selecionado

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,6 +1,7 @@
 package candidatura;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
@@ -11,6 +12,7 @@ public class ProcessoSeletivo {
     static void selecaoCandidatos() {
         String[] candidatos = {"Guilherme", "Carlos", "Enrico", "Nader", "Marcelo", "Pedro", "Arthur", "Henrique", "Leonardo", "Andrey"};
         ArrayList<String> listaSelecionados = new ArrayList<>();
+        ArrayList<String> listaAprovados = new ArrayList<>();
         int candidatosSelecionados = 0;
         int candidatoAtual = 0;
         double salarioBase = 2000.00;
@@ -32,25 +34,56 @@ public class ProcessoSeletivo {
             candidatoAtual++;
         }
 
-        System.out.println(listaSelecionados.size() + " candidatos selecionados para a vaga: " + listaSelecionados.toString().replace("[", "").replace("]", "."));
+        if (listaSelecionados.size() != 0){
+            if (listaSelecionados.size() == 1) {
+                System.out.println(listaSelecionados.size() + " candidato selecionado para a vaga: " + listaSelecionados.toString().replace("[", "").replace("]", ".") + "\n");
+            }
+            else {
+                System.out.println(listaSelecionados.size() + " candidatos selecionados para a vaga: " + listaSelecionados.toString().replace("[", "").replace("]", ".") + "\n");
+            }
+
+        }
+        else {
+            System.out.println("Nenhum candidato foi selecionado para a vaga.\n");
+        }
+
+        listaAprovados = entrarEmContato(listaSelecionados, listaAprovados);
+
+        if (listaAprovados.size() != 0) {
+            if (listaAprovados.size() == 1){
+                System.out.println("\n" + listaAprovados.size() + " candidato aprovado: " + listaAprovados.toString().replace("[", "").replace("]", ".") + "\n");
+            }
+            else {
+                System.out.println("\n" + listaAprovados.size() + " candidatos aprovados: " + listaAprovados.toString().replace("[", "").replace("]", ".") + "\n");
+            }
+        }
+        else {
+            System.out.println("\nNenhum candidato foi aprovado.");
+        }
     }
 
     static double randomizarValorPretendido(){
-        double value = ThreadLocalRandom.current().nextDouble(1700.00, 2400.99);
-        return value;
+        return ThreadLocalRandom.current().nextDouble(1700.00, 2400.99);
     }
 
-    static void analisarCandidato(double salarioPretendido) {
-        double salarioBase = 2000.00;
+    static ArrayList<String> entrarEmContato(ArrayList<String> selecionados, ArrayList<String> aprovados){
+        for (String selecionado : selecionados){
+            Random random = new Random();
+            int tentativas = 0;
+            boolean atendeu = false;
 
-        if (salarioBase > salarioPretendido) {
-            System.out.println("LIGAR PARA O CANDIDATO.");
+            while (tentativas < 3 && !atendeu) {
+                atendeu = random.nextBoolean();
+                tentativas++;
+                if (atendeu){
+                    aprovados.add(selecionado);
+                    System.out.println("Conseguimos contato com " + selecionado + " na " + tentativas + "º tentativa");
+                }
+                if (tentativas >= 3 && !atendeu){
+                    System.out.println("Não conseguimos contato com " + selecionado + " após " + tentativas + " tentativas");
+                }
+            }
         }
-        else if (salarioBase == salarioPretendido){
-            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA.");
-        }
-        else {
-            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS.");
-        }
+        return aprovados;
     }
 }
