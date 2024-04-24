3ed60400405f6c20ac8ce794fe3881fe4b889cf2
guigx <guigo211103@gmail.com>
2024-04-24 15:05:31 -0300

    Selecionando Candidatos

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,16 +1,46 @@
 package candidatura;
 
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
+        selecaoCandidatos();
+    }
 
-        analisarCandidato(1200.00);
-        analisarCandidato(2900.00);
-        analisarCandidato(2000.00);
+    static void selecaoCandidatos() {
+        String[] candidatos = {"Guilherme", "Carlos", "Enrico", "Nader", "Marcelo", "Pedro", "Arthur", "Henrique", "Leonardo", "Andrey"};
+        ArrayList<String> listaSelecionados = new ArrayList<>();
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.00;
 
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = randomizarValorPretendido();
+
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário: R$ " + String.format("%.2f", salarioPretendido));
+            if (salarioBase >= Double.valueOf(salarioPretendido)) {
+                listaSelecionados.add(candidato);
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga\n");
+                candidatosSelecionados++;
+            }
+            else {
+                System.out.println("O candidato " + candidato + " não foi selecionado para a vaga\n");
+            }
+            candidatoAtual++;
+        }
+
+        System.out.println(listaSelecionados.size() + " candidatos selecionados para a vaga: " + listaSelecionados.toString().replace("[", "").replace("]", "."));
+    }
+
+    static double randomizarValorPretendido(){
+        double value = ThreadLocalRandom.current().nextDouble(1700.00, 2400.99);
+        return value;
     }
 
     static void analisarCandidato(double salarioPretendido) {
-
         double salarioBase = 2000.00;
 
         if (salarioBase > salarioPretendido) {
